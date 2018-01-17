78a2883de6a41fc68c6c660ab57743f9cd273d1f
aayush1408 <aayushjaiswal0114@gmail.com>
2018-01-17 20:11:28 +0530

    display the list

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,6 +2,28 @@
     <div>
       <p>Completed task: {{  todos.filter((todo) => { return todo.done===true }).length }}</p>
       <p>Incompleted task: {{ todos.filter((todo) =>{return todo.done===false}).length }}</p>
+      <div class='ui centered card' v-for="todo in todos">
+        <div class="content">
+          <div class="header">
+            {{todo.title}}
+          </div>
+          <div class="meta">
+            {{todo.project}}
+          </div>
+          <div class="extra content">
+            <span class="right floated edited icon">
+              <i class="edit icon"></i>
+            </span>
+          </div>         
+        </div>
+        <div class='ui bottom attached green basic button' v-show="todo.done">
+        Completed
+        </div>
+        <div class='ui bottom attached red basic button' v-show="!todo.done">
+        Complete
+        </div>
+      </div>
+
     </div>
 </template>
 
