565f5807e15753bfba212eb91a527974cb97347d
aayush1408 <aayushjaiswal0114@gmail.com>
2018-01-17 20:21:41 +0530

    cleaned the code

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,34 +2,18 @@
     <div>
       <p>Completed task: {{  todos.filter((todo) => { return todo.done===true }).length }}</p>
       <p>Incompleted task: {{ todos.filter((todo) =>{return todo.done===false}).length }}</p>
-      <div class='ui centered card' v-for="todo in todos">
-        <div class="content">
-          <div class="header">
-            {{todo.title}}
-          </div>
-          <div class="meta">
-            {{todo.project}}
-          </div>
-          <div class="extra content">
-            <span class="right floated edited icon">
-              <i class="edit icon"></i>
-            </span>
-          </div>         
-        </div>
-        <div class='ui bottom attached green basic button' v-show="todo.done">
-        Completed
-        </div>
-        <div class='ui bottom attached red basic button' v-show="!todo.done">
-        Complete
-        </div>
-      </div>
-
+      <todo  v-for="todo in todos" v-bind:todo="todo"></todo>
     </div>
 </template>
-
+import Todo from './Todo.vue';
 <script type="text/javascript">
+import Todo from './Todo.vue';
+
     export default{
-        props:['todos']
+        props:['todos'],
+        components:{
+          Todo
+        }
     };
 </script>
 
