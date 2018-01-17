33ca870da9c1d2705eb03bb465f463e571559f78
aayush1408 <aayushjaiswal0114@gmail.com>
2018-01-17 21:53:46 +0530

    Completed status

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,7 @@
     <div>
       <p>Completed task: {{  todos.filter((todo) => { return todo.done===true }).length }}</p>
       <p>Incompleted task: {{ todos.filter((todo) =>{return todo.done===false}).length }}</p>
-      <todo  v-on:delete-todo="deleteTodo"  v-for="todo in todos" v-bind:todo="todo"  ></todo>
+      <todo  v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" v-bind:todo="todo"  ></todo>
     </div>
 </template>
 import Todo from './Todo.vue';
@@ -18,7 +18,11 @@ import Todo from './Todo.vue';
           deleteTodo(todo) {
             const todoIndex = this.todos.indexOf(todo);
             this.todos.splice(todoIndex, 1);
-          }
+          },
+          completeTodo(todo) {
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos[todoIndex].done = true;
+        }
         }
     };
 </script>
