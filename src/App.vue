c76fc7a9f95d65e901932c44064de1fc839126e7
aayush1408 <aayushjaiswal0114@gmail.com>
2018-01-17 19:39:41 +0530

    intial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <todo-list></todo-list> 
+  </div>
+</template>
+import TodoList from './components/TodoList.vue';
+
+<script>
+import TodoList from './components/TodoList.vue';
+export default {
+  components:{
+    TodoList
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
